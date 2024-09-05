b6f822d89178341c1ce7b9b02befa19aa3a3fa3e
asadbabayev <asadyb@code.edu.az>
2024-09-05 15:25:24 +0400

    save changes

--- components/layout/footer/index.tsx
+++ components/layout/footer/index.tsx
@@ -13,7 +13,6 @@ const Footer = () => {
       <BlankSection />
       <SubFooter />
       <FooterMain />
-  
     </footer>
   );
 };
--- components/layout/footer/subFooter/index.tsx
+++ components/layout/footer/subFooter/index.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 
+// Container
+import Container from "@/main-container";
+
+
+
+// Icons
+import Email from "@/assets/images/icons/Vector.png"
+import Image from "next/image";
+
 const SubFooter = () => {
   return (
-    <section className="main">
-      <div className="container"></div>
+    <section>
+      <Container>
+        <div className="flex justify-between px-16 py-10 bg-black rounded-[20px]">
+          {/* left */}
+          <div>
+            <p className="uppercase font-bold text-white text-4xl">STAY UPTO DATE ABOUT OUR LATEST OFFERS</p>
+          </div>
+          {/* right */}
+          <div>
+            {/* email */}
+            <div>
+              <Image src={Email} w=[20] h=[15] alt="emailIcon" />
+            </div>
+            {/* button */}
+            <div>
+              <button></button>
+            </div>
+          </div>
+        </div>
+      </Container>
     </section>
   );
 };
