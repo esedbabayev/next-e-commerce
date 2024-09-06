f6ec0e11f1733a5a66b8458709e196fb94893e67
Asad Babayev <esedbabayev12@gmail.com>
2024-09-07 02:53:33 +0400

    delete comments

--- components/main-container/index.tsx
+++ components/main-container/index.tsx
@@ -4,11 +4,6 @@ interface AuxProps {
   children: JSX.Element;
 }
 
-// const containerStyles = {
-//   maxWidth: "358px",
-//   marg
-// }
-
 const Container: React.FC<AuxProps> = ({ children }) => {
   return (
     <div className="max-w-[1240px] px-4 mx-auto">
