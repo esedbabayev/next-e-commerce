36b52cf9edf9d99ca6dd4f35e72a5a8a0cb64cec
asadbabayev <asadyb@code.edu.az>
2024-09-06 17:32:36 +0400

    change directory

--- components/main-container/index.tsx
+++ components/main-container/index.tsx
@@ -4,8 +4,17 @@ interface AuxProps {
   children: JSX.Element;
 }
 
+// const containerStyles = {
+//   maxWidth: "358px",
+//   marg
+// }
+
 const Container: React.FC<AuxProps> = ({ children }) => {
-  return <div className="w-full max-w-[1240px] mx-auto">{children}</div>;
+  return (
+    <div className="max-w-[1240px] px-4 mx-auto">
+      {children}
+    </div>
+  );
 };
 
 export default Container;
