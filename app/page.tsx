2335a519cf9f4f705817c70e4d4be5ba2912d041
Asad Babayev <esedbabayev12@gmail.com>
2024-09-09 15:31:54 +0400

    save changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
-  return (
-    <div>
-      homeasadssd
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default HomePage
+export default HomePage;
