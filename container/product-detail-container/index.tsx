df225441215c21dc7ccdc72fa8b04cf1c7e85bf0
Ulkar Aghayeva <ulkarragh@code.edu.az>
2024-09-16 00:50:46 +0400

    save changes

---
+++ container/product-detail-container/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// Components
+import ProductDetails from '@/components/pages/ProductDetailPage/productDetail'
+import Reviews from '@/components/pages/ProductDetailPage/reviews'
+
+const ProductDetailContainer = () => {
+  return (
+    <main>
+        <ProductDetails/>
+        <Reviews/>
+        
+        
+    </main>
+  )
+}
+
+export default ProductDetailContainer
