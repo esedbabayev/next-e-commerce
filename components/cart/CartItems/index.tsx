4e83d2e5304a53e32d10615c0b7333caee55ed5a
Asad Babayev <esedbabayev12@gmail.com>
2024-09-18 01:56:54 +0400

    reconstruct & add responsivity

--- components/cart/CartItems/index.tsx
+++ components/cart/CartItems/index.tsx
@@ -4,13 +4,8 @@ import CartItem from "./CartItem";
 
 const CartItems = () => {
   return (
-    <div className="px-4 flex flex-col gap-5">
-      <div>
-        <h1 className="uppercase font-bold text-3xl font-integral">
-          Your cart
-        </h1>
-      </div>
-      <div className="flex flex-col gap-4 p-[14px] border border-[#0000001A] rounded-[20px] max-h-[390px] overflow-x-hidden overflow-y-visible ">
+    <div className="px-4">
+      <div className="flex flex-grow flex-col gap-4 p-[14px] border border-[#0000001A] rounded-[20px] max-h-[410px] md:max-h-[508px] overflow-x-hidden overflow-y-auto">
         <CartItem />
         <CartItem />
         <CartItem />
