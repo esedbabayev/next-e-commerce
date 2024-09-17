ed2b119cf63cfbd4990616ffe25279cf5520951a
Asad Babayev <esedbabayev12@gmail.com>
2024-09-18 01:56:25 +0400

    make adjustments

--- components/cart/CartItems/CartItem/index.tsx
+++ components/cart/CartItems/CartItem/index.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import TShirt from "@/assets/cart/Frame 33.png";
 import TrashIcon from "@/assets/cart/Frame.png";
 import Minus from "@/assets/cart/Frame (1).png";
+import Plus from "@/assets/cart/Plus.png";
 
 const CartItem = () => {
   return (
@@ -30,17 +31,19 @@ const CartItem = () => {
               Color: <span className="font-light">White</span>
             </p>
           </div>
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             {/* price */}
             <div>
               <p className="font-bold text-xl">$145</p>
             </div>
-            <div className="bg-[#F0F0F0] py-3 px-5 rounded-[62px]">
+            <div className="flex gap-5 items-center bg-[#F0F0F0] py-3 px-5 rounded-[62px]">
               <span>
-                <Image src={Minus} alt="minus" width={16} height={16}/>
+                <Image src={Minus} alt="minus" width={16} height={16} />
               </span>
               <span>1</span>
-              <span>+</span>
+              <span>
+                <Image src={Plus} alt="plus icon" width={16} height={16} />
+              </span>
             </div>
           </div>
         </div>
