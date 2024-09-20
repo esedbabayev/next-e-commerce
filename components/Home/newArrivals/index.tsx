cc44efe16b74d2c96abc82cf5d887e3a221c8662
Ulkar Aghayeva <ulkarragh@code.edu.az>
2024-09-20 17:40:08 +0400

    save changes

--- components/Home/newArrivals/index.tsx
+++ components/Home/newArrivals/index.tsx
@@ -1,50 +1,44 @@
+import React, { useEffect, useState } from "react";
+import "./style.css";
 
-import React, { useEffect, useState } from 'react'
-import "./style.css"
-
-import Container from '@/components/main-container'
+import Container from "@/components/main-container";
 
 // Components
-import NewProduct from './newProduct'
-import { IProduct } from '@/types/product'
+import NewProduct from "./newProduct";
+import { IProduct } from "@/types/product";
 
-interface IProps{
+interface IProps {
   title: string;
-  products:IProduct[],
+  products: IProduct[];
 }
-const NewArrivals:  React.FC<IProps> = ({products, title }) => {
+const NewArrivals: React.FC<IProps> = ({ products, title }) => {
+  return (
+    <section>
+      <Container>
+        <div className="max-w-[1240px] mx-auto  flex flex-col items-center justify-center px-4 mt-[50px]">
+          <div className="flex items-center justify-center">
+            <h1 className="text-[32px] text-center font-bold font-integral mb-3 lg:mb-10">
+              {title}
+            </h1>
+          </div>
 
-return (
+          <div
+            className="space-x-4 w-full overflow-x-visible 
+     overflow-y-hidden flex "
+          >
+            {products &&
+              products.map((newArrival) => {
+                return <NewProduct newArrival={newArrival} />;
+              })}
+          </div>
+          <div className="py-4 px-13 mt-6 mb-10 border border-[#0000001A] rounded-full flex justify-center w-full lg:w-[17%] lg:mb-16">
+            <button className="text-[14px] font-medium ">View All</button>
+          </div>
+          <hr className="border border-[#0000001A] w-full mb-10 lg:mb-16" />
+        </div>
+      </Container>
+    </section>
+  );
+};
 
-<section>
-    <Container>
-    <div className='max-w-[1240px] mx-auto  flex flex-col items-center justify-center px-4 mt-[50px]'>
-    <div className='flex items-center justify-center'>
-        <h1 className='text-[32px] text-center font-bold font-integral mb-3 lg:mb-10'>{title}</h1>
-    </div>
-     
-     <div className='space-x-4 w-full whitespace-nowrap overflow-x-visible 
-     overflow-y-hidden flex lg:overflow-x-hidden lg:grid grid-cols-4' >
-      {
-        products && products.map((newArrival) => {
-          return <NewProduct newArrival={newArrival} />
-        })
-      }
-
-     </div>
-
-     <div className='py-4 px-13 mt-6 mb-10 border border-[#0000001A] rounded-full flex justify-center w-full lg:w-[17%] lg:mb-16'>
-        <button className='text-[14px] font-medium '>View All</button>
-      </div>
-
-      <hr className='border border-[#0000001A] w-full mb-10 lg:mb-16' />
-
-    </div>
-    </Container>
-
-</section>
-
-  )
-}
-
-export default NewArrivals
+export default NewArrivals;
